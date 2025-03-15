eb9155e9da3b5333fecd41da00ecba72a58523f2
Leonardo-Bartolacci <bartolz2003@gmail.com>
2025-03-15 21:33:33 +0100

    Aggiunta delle classi C++ "CentralRoomGenerator" e "RoomGenerator"

---
+++ wizard_test/Source/wizard_test/CentralRoomGenerator.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CentralRoomGenerator.h"
+
+// Sets default values
+ACentralRoomGenerator::ACentralRoomGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACentralRoomGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACentralRoomGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
