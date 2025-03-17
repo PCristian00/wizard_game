bcd9f3037454348fe02a9c56cb0866fb4db5f903
Leonardo-Bartolacci <bartolz2003@gmail.com>
2025-03-17 09:15:44 +0100

    Riscrittura del codice definitivo per la classe "Portal"

--- wizard_test/Source/wizard_test/Portal.cpp
+++ wizard_test/Source/wizard_test/Portal.cpp
@@ -6,8 +6,24 @@
 // Sets default values
 APortal::APortal()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	//const ConstructorHelpers::FObjectFinder<UStaticMesh> MeshObj(TEXT("/Game/ThirdPerson/Maps/_GENERATED/barto/SMesh_portal"));
+	const ConstructorHelpers::FObjectFinder<UStaticMesh> MeshObj(TEXT("/Game/LevelPrototyping/Meshes/SM_ChamferCube"));
+
+	Mesh->SetStaticMesh(MeshObj.Object);
+
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->SetRelativeLocation(GetActorLocation() + FVector(0, 0, 115));
+	Trigger->SetRelativeScale3D(FVector(3.2, 2.35, 3.25));
 
 }
 
@@ -15,7 +31,8 @@ APortal::APortal()
 void APortal::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	OnActorBeginOverlap.AddDynamic(this, &APortal::OnTriggerOverlap);
 }
 
 // Called every frame
